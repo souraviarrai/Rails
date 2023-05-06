bb76db8bd4b760da4713f0acae12cb8d114c212b
souravdungbungrai <souravdungbungrai@gmail.com>
2023-05-06 20:38:17 +0600

    newest

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  has_many :comments
+
   validates :title, presence: true, length: { minimum: 5, maximum: 10 }
   validates :body, presence: true, length: { minimum: 10, maximum: 40 }
 end
